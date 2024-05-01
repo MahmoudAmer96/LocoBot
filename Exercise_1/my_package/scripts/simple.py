427911f8aa5a7d9fdc26b43eb4c371b0317d6098
aezm <526392531@qq.com>
2024-05-01 11:00:29 +0200

    update

--- Exercise_1/my_package/scripts/simple.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-
-import rospy
-
-rospy.init_node('ObiWan')
-rate = rospy.Rate(2)
-while not rospy.is_shutdown():
-    print("Help me Obi-Wan Kenobi, you're my only hope")
-    rate.sleep()
